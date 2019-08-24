db29c07b8bfc2141c0cb6a2c3d3b0604b04d1e2b
Maksim Sandybekov <maksim.sandybekov@live.de>
2019-08-24 23:28:23 +0200

    initial commit 2019-08-24

---
+++ lib/pow.ex
@@ -0,0 +1,18 @@
+defmodule Pow do
+  @moduledoc """
+  Documentation for Pow.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pow.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
