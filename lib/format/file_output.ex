b59018629dc654d411f4c318cb74c6791f8deb4d
Maksim Sandybekov <maksim.sandybekov@live.de>
2019-09-05 01:06:52 +0200

    add functions to write csv files

--- lib/format/file_output.ex
+++ lib/format/file_output.ex
@@ -1,17 +1,58 @@
 defmodule Exp.Format.FileOutput do
+    alias Exp.Format.Config
+
     @moduledoc """
         Formatting the export of entry tables to output files.
     """
 
+    def write_file(data, opts \\ [format: :csv, dir: "./"]) do
 
-    def format_content(content_array, format, agg \\ "")
-
-    def format_content([], :csv, agg), do: agg
-    def format_content([h| []], :csv, agg), do: agg <> h
-    def format_content([h| t], :csv, agg) do
-        tmp_result = agg <> h <> ","
-        format_content(t, :csv, tmp_result)
     end
 
+
+    @doc """
+        Parses the given data to the output format.
+
+        Parameters
+        - output_format: atom representing the output format [:csv | :json | :xml]
+        - data: given entry data
+        
+    """
+    def format(type, data, opts \\ [sep: ",", field_names: []])
+    def format(:csv = type, data, opts) do
+        content = ""
+        
+        if is_list(data) || is_map(data) || is_tuple(data) do
+            type |> resolve(data)s
+        else
+
+        end
+    end
+
+    def format(:json, data, opts) do
+        
+    end
+
+    def format(:xml, data, opts) do
+        
+    end
+
+
+    @doc """
+
+        Parameters:
+        - output_format: type of output [:csv | :json | :xml]
+    """
+    def resolve(type, data, acc \\ [], opts \\ [])
+    def resolve(_, [], acc), do: acc
+    def resolve(:csv, [field| rest], acc) do
+        
+    end
+
+
+    # XML|CSV specific
+    def create_header(type, keys) do
+        
+    end
 
 end
--- test/format/file_output_test.exs
+++ test/format/file_output_test.exs
@@ -5,7 +5,40 @@ defmodule TestExpFormatFileOutput do
 
 
     describe "test/csv-output" do
-        
+
+        test "single/integer" do
+            assert FileOutput.format(:csv, 22) == "21"
+        end
+
+        test "single/boolean" do
+            assert FileOutput.format(:csv, true) == "none"
+        end
+
+        test "collection/tuple" do
+            assert FileOutput.format(:csv, {1, 2}) == "1,2"
+        end
+
+        test "nested/tuple" do
+            assert FileOutput.format(:csv, {1, {2, 3}}) == "1,[2,3]"
+        end
+
+        test "collection/list" do
+            assert FileOutput.format(:csv, [1, 2]) == "1,2"
+        end
+
+        test "nested/list" do
+            assert FileOutput.format(:csv, [1, [2, 3]]) == "1,[2,3]"            
+        end
+
+        test "valid-opt-separator" do
+            assert FileOutput.format(:csv, [1, 2, 3], [sep: ";"]) == "1;2;3"
+        end
+
+        test "invalid-separator" do
+            assert FileOutput.format(:csv, [1, 3, 4], [sep: true]) == false
+        end
+
+    
     end
 
 
